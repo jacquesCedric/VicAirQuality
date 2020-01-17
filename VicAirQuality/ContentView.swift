8779b81ba4afe4c9d12c2e3ab2f27f2f6d40af07
Jacob <12831497+jacquesCedric@users.noreply.github.com>
2020-01-18 10:08:52 +1100

    Initial Commit

---
+++ VicAirQuality/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  VicAirQuality
+//
+//  Created by Jacob Gold on 18/1/20.
+//  Copyright © 2020 Jacob Gold. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
